1533d96081900f9c2232eaebaefb64dea42d4d37
pplgjulio <pplgjulio@gmail.com>
2024-12-02 11:31:50 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1733113598,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/tracker-sheet0.png",
+		"images/respoint1-sheet0.png",
+		"images/barrier-sheet0.png",
+		"images/suppressor-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
